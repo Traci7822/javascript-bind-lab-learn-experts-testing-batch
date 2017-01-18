c119beee255ecfb1fd99a633bbeec9314d58b5bd
Traci7822 <Tracir@gmail.com>
2017-01-18 09:01:12 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much.";
+const digitalClock = {
+  time: Math.round(new Date().getTime() / 1000),
+  startTicking: function (){
+    setInterval(() => this.time++, 1000);
+  }
+};
+
+function censor(word, string) {
+  const regWord = new RegExp(word, "gi")
+  return string.replace(regWord, "BLEEP");
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');
